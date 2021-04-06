1b6813da224e022d5ff38966387a1d6709700191
zhujialiang <zhujialiang@aibaoxian.com>
2021-04-06 16:07:21 +0800

    fix

---
+++ precache-manifest.89510e242eb6c8603ea8ed75684e2671.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e4ffcfef82232eabc16e782c67e0276",
+    "url": "/todolist-hqy/index.html"
+  },
+  {
+    "revision": "4f5fd5aaa83ce7e5acb9",
+    "url": "/todolist-hqy/static/css/main.d611f5bc.chunk.css"
+  },
+  {
+    "revision": "061a7c29ed7a8b54dd4e",
+    "url": "/todolist-hqy/static/js/2.afd96761.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todolist-hqy/static/js/2.afd96761.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f5fd5aaa83ce7e5acb9",
+    "url": "/todolist-hqy/static/js/main.1581ab3c.chunk.js"
+  },
+  {
+    "revision": "d8a8f93e4d61b1fe4d05",
+    "url": "/todolist-hqy/static/js/runtime-main.ef4e8dcb.js"
+  }
+]);
